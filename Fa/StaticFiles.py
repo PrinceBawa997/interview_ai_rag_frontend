b3096d9c4ce58d3aea840dc1c45ca940deb32e74
Prince Bawa <bawaprince722@gmail.com>
2026-06-04 23:29:26 +0530

    clean project structure for railway deploy

--- Fa/StaticFiles.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
-from starlette.templating import Jinja2Templates
-
-app = FastAPI()
-app.mount("/static",StaticFiles(directory="../static"), name="static")
-templates = Jinja2Templates(directory="../templates")
--- Fa/main1.py
+++
@@ -1,8 +0,0 @@
-#creating dynamic routing in the fastapi |
-
-from fastapi import FastAPI
-app = FastAPI()
-
-@app.get("/blog/{user_name}")
-def blogpage(user_name):
-    return {"page":"blogpage","user_name":user_name}
